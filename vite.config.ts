70cb200bd4a9538c9509b1942a058869d2524c65
wangbing56 <12267007+wangbing56@user.noreply.gitee.com>
2024-05-30 16:14:11 +0800

    第一次提交代码

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+/**
+ * 一般用来配置我们服务的一些配置东西
+ * 比如代理，启动项目的名称
+ */
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
