93f0b77180f1780f3959deffbd5a9c6d9a2ee94f
wangbing56 <12267007+wangbing56@user.noreply.gitee.com>
2024-05-31 17:11:37 +0800

    更改了utils文件

---
+++ src/utils/auth.ts
@@ -0,0 +1,24 @@
+/**
+ * 将token封装到cookie中
+ * 
+ * 1.设置到cookie中  set
+ * 2.用的时候从cookie中拿到  get
+ * 的那我们推出的时候，将token从cookie中移出 remove
+ */
+import Cookies from 'js-cookie'
+/**
+ * 设置cookies
+ */
+let token = 'token'
+function setToken(value: string) {
+    Cookies.set(token, value)
+}
+function getToken() {
+    return Cookies.get(token)
+}
+function removeToken() {
+    Cookies.remove(token)
+}
+export default {
+    setToken, getToken, removeToken
+}
