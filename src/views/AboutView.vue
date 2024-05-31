a4af82bba9d72d13bea2f6809e1c74fe017529d6
wangbing56 <12267007+wangbing56@user.noreply.gitee.com>
2024-05-31 11:26:22 +0800

    第三次提交

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,9 +3,7 @@
     <h1>This is an about page</h1>
   </div>
 </template>
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 <style>
 @media (min-width: 1024px) {
   .about {
